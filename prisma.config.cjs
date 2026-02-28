1b38d18170f84d2918d1b47ddc0d63832780a8ef
Lucifer-Aspire-A315 <omkaromu183@gmail.com>
2026-02-28 16:19:16 +0530

    small bug fix

--- prisma.config.cjs
+++ prisma.config.cjs
@@ -1,11 +1,8 @@
 /* eslint-disable no-undef */
 require('dotenv').config();
 
-console.log('DATABASE_URL value:', process.env.DATABASE_URL);
-console.log('DATABASE_URL type :', typeof process.env.DATABASE_URL);
-
 module.exports = {
   datasource: {
-    url: url(process.env.DATABASE_URL),
+    url: process.env.DATABASE_URL,
   },
 };
